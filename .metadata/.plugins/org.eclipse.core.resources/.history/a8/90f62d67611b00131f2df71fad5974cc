01af75891a09775192a896c4e09c0339f2586576
Scott Caruso <scottcaruso@fullsail.edu>
2013-09-12 00:47:32 -0400

    Parses json data for location and state name; passes this data to the next view; next view handles and displays.

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/a8/90f62d67611b00131f2df71fad5974cc
@@ -0,0 +1,8 @@
+package com.scottcaruso.hereiam;
+
+import android.app.Activity;
+
+public class DisplayActivity extends Activity 
+{
+
+}
--- HereIAm/gen/com/scottcaruso/hereiam/R.java
+++ HereIAm/gen/com/scottcaruso/hereiam/R.java
@@ -182,16 +182,21 @@ containing a value of this type.
         public static final int here_i_am_icon=0x7f020018;
         public static final int here_i_am_logo=0x7f020019;
         public static final int ic_launcher=0x7f02001a;
+        public static final int placeholder=0x7f02001b;
     }
     public static final class id {
-        public static final int action_settings=0x7f050007;
+        public static final int action_settings=0x7f05000b;
+        public static final int capturedImage=0x7f050007;
+        public static final int facebookbutton=0x7f050009;
         public static final int findmebutton=0x7f050005;
         public static final int hybrid=0x7f050004;
         public static final int logo=0x7f050006;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
         public static final int satellite=0x7f050002;
+        public static final int statusUpdate=0x7f050008;
         public static final int terrain=0x7f050003;
+        public static final int twitterbutton=0x7f05000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -283,6 +288,9 @@ containing a value of this type.
         public static final int common_signin_button_text_long=0x7f06000e;
         public static final int find_me_now_=0x7f060018;
         public static final int hello_world=0x7f060017;
+        public static final int placeholder_status_update_string_=0x7f060019;
+        public static final int post_to_facebook=0x7f06001a;
+        public static final int post_to_twitter=0x7f06001b;
     }
     public static final class style {
         /** 
